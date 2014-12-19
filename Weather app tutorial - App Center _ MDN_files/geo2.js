e93534ef99fd7a4607b5f549243dc662d692542b
Don Ricardo Jr <dricardo1@gmail.com>
2014-12-19 13:58:24 -0500

    Initial commits

---
+++ Weather app tutorial - App Center _ MDN_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "ZEPHYRHILLS",
+      'continent': "NA",
+      'country': "US",
+      'region': "FL"
+    },
+    'ip':"71.100.57.253"
+  }]);
+})
+//
+()
+
+;
